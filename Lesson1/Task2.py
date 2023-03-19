6c7b54fc85607f80e22a8aef4ca482a164ecc759
AlterBins <sashap84@mail.ru>
2023-03-20 01:08:58 +0300

    Deleting Lesson1

--- Lesson1/Task2.py
+++
@@ -1,7 +0,0 @@
-# Задание 2
-
-count_sec = int(input("введите время в секундах: "))
-min_sec = count_sec / 60
-hour_sec = count_sec / 3600
-print(f"время в формате ч:м:с - {round(hour_sec, 2)}:{round(min_sec, 2)}:"
-      f"{count_sec}")
--- Lesson1/Task3.py
+++
@@ -1,7 +0,0 @@
-# Задание 3
-
-n = int(input("введите целое положительное число n: "))
-nn = n * 10 + n
-nnn = n * 100 + nn
-sum_n = n + nn + nnn
-print(f"cумма чисел {n} + {nn} + {nnn} = {sum_n}")
