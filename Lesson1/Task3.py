5cab96642145fb76efe6f7e7f32fac99142f089b
AlterBins <sashap84@mail.ru>
2023-03-19 22:29:56 +0300

    New print

--- Lesson1/Task3.py
+++ Lesson1/Task3.py
@@ -4,4 +4,4 @@ n = int(input("введите целое положительное число n
 nn = n * 10 + n
 nnn = n * 100 + nn
 sum_n = n + nn + nnn
-print(f"сумма чисел n + nn + nnn = {sum_n}")
+print(f"cумма чисел {n} + {nn} + {nnn} = {sum_n}")
